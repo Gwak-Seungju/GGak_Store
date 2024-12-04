8590b4907ed96f62664cd26a934addbeaca7aa85
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-12-05 04:18:32 +0900

    feat: 서버 연결 및 svg 컴포넌트화

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 3000,
+    proxy: {
+      '/v1': {
+        target: 'https://openapi.naver.com',
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [react(), svgr({ include: '**/*.svg' })],
+});
