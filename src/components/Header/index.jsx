6791f5ae6bbf3184c62acb4f6739ad492d383aaf
Gwak-Seungju <74342515+Gwak-Seungju@users.noreply.github.com>
2024-12-05 11:26:43 +0900

    feat: 헤더 추가

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,46 @@
+import useStore from '/src/utils/store/store';
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import SearchIcon from '/src/assets/search-icon.svg';
+import BucketIcon from '/src/assets/bucket-icon.svg';
+import styles from './Header.module.scss';
+
+function Header() {
+  const { bucket } = useStore((state) => state);
+  const inputRef = useRef();
+  const navigate = useNavigate();
+
+  const activeEnter = (e) => {
+    if (e.key === 'Enter') {
+      navigate(`/SearchPage/${inputRef.current.value}`);
+    }
+  };
+  const goHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className={styles.header}>
+      <div className={styles['header__main-inner']}>
+        <h1 className={styles['header__main-innner--name']} onClick={goHome}>
+          GGak Store
+        </h1>
+        <div className={styles['header__main-inner--search']}>
+          <input placeholder="상품 입력" type="text" ref={inputRef} onKeyDown={(e) => activeEnter(e)} />
+          <SearchIcon />
+        </div>
+        <div
+          className={styles.bucket}
+          onClick={() => {
+            navigate('/BucketPage');
+          }}
+        >
+          <div className={styles.bucket__count}>{bucket.length}</div>
+          <BucketIcon className={styles.bucket__icon} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
